17f1702b61cd0d22e10aab1ffa29f84e59b04372
Rafael Jordão <79488603+RafaelJordao06@users.noreply.github.com>
2023-11-23 13:50:48 -0300

    tratando erros e melhorias

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-function performSearch() {
-  // Obter o valor do campo de entrada
+document
+  .getElementById("codigo")
+  .addEventListener("keypress", function (evento) {
+    if (evento.key === "Enter") {
+      performSearch();
+    }
+  });
 
+function performSearch() {
+  var inputCodigo = document.getElementById("codigo");
+  // Obter o valor do campo de entrada
   var trackingCode = document.getElementById("codigo").value;
 
+  // Validação do código de rastreio
+  if (!validarCodigoRastreio(trackingCode)) {
+    alert("Código de rastreio inválido.");
+    inputCodigo.value = ""; // Limpar o campo de input
+    return; // Encerrar a função se o código for inválido
+  }
   trackingCode = trackingCode.replace(/\s+/g, "").toUpperCase();
+
+  document.getElementById("loadingScreen").style.display = "flex";
 
   // Fazer uma requisição para o servidor Node.js
   fetch(
@@ -13,19 +29,29 @@ function performSearch() {
     .then((data) => {
       processarEventos(data.eventos);
       console.log(data);
+      console.log(data.eventos.length);
+      document.getElementById("loadingScreen").style.display = "none";
     })
     .catch((error) => {
       console.error("Erro ao rastrear:", error);
+      document.getElementById("loadingScreen").style.display = "none";
     });
 }
 
 function processarEventos(eventos) {
   var container = document.getElementById("eventos-container");
   container.innerHTML = ""; // Limpa o conteúdo anterior
-
-  eventos.forEach(function (evento) {
-    container.innerHTML += criarHtmlEvento(evento);
-  });
+  if (eventos.length > 0) {
+    eventos.forEach(function (evento) {
+      container.innerHTML += criarHtmlEvento(evento);
+    });
+  } else {
+    return (container.innerHTML += `
+    <div class="tracking-status-container">
+      <p class="tracking-status-title">Nenhum evento encontrado com esse código de rastreio</p>
+    </div>
+  `);
+  }
 }
 
 function criarHtmlEvento(evento) {
@@ -65,5 +91,23 @@ function selecionarIcon($status) {
     "Revisão de tributos solicitada pelo cliente": "revisaotributos.svg",
   };
 
-  return $mapeamentoIcones[$status] || "Caminhao.svg";
+  return $mapeamentoIcones[$status] || "caminhao.svg";
+}
+
+function validarCodigoRastreio(trackingCode) {
+  // Remover espaços e converter para maiúsculas
+  trackingCode = trackingCode.replace(/\s+/g, "").toUpperCase();
+
+  // Padrões comuns
+  const padraoCorreiosBR = /^[A-Z]{2}\d{9}[A-Z]{2}$/; // Ex: AA123456789BR
+  const padraoUSPS = /^(\d{20,22}|[A-Z]{2}\d{9}[A-Z]{2})$/; // Ex: 9400110200830500000000 ou EA123456789US
+  const padraoFedEx = /^(\d{12}|\d{15}|\d{20})$/; // Ex: 123456789012
+  const padraoDHL = /^\d{10,11}$/; // Ex: 1234567890
+
+  return (
+    padraoCorreiosBR.test(trackingCode) ||
+    padraoUSPS.test(trackingCode) ||
+    padraoFedEx.test(trackingCode) ||
+    padraoDHL.test(trackingCode)
+  );
 }
